fb44c86b27da380c663076e3f89643ba55fffa5d
Yonathan Castillo <leothan522@gmail.com>
2022-04-23 20:13:01 -0400

    first commit

---
+++ app/Http/Controllers/Dashboard/SearchController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SearchController extends Controller
+{
+    public function showNavbarSearchResults(Request $request)
+    {
+        //  Comprobar que la palabra clave de búsqueda está presente.
+
+        if (! $request->filled('searchVal')) {
+            return back();
+        }
+
+        // Obtener la palabra clave de búsqueda.
+
+        $keyword = $request->input('searchVal');
+
+        //obtiene ruta anterior
+        $route = redirect()->getUrlGenerator()->previous();
+
+        //chequeamos las rutas
+        if (strpos($route, '/dashboard/usuarios') !== false){
+            //verSweetAlert2("Resultados encontrados", 'toast');
+            return redirect()->route('usuarios.index', $keyword);
+        }
+
+        if (strpos($route, '/dashboard/parametros') !== false){
+            //verSweetAlert2("Resultados encontrados", 'toast');
+            return redirect()->route('parametros.index', $keyword);
+        }
+
+        //en caso de no encontrar ninguna ruta
+        verSweetAlert2("Opcion no encontrada", 'toast');
+        return back();
+
+    }
+}
